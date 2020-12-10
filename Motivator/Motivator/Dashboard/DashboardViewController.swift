9b4a79cdaa99305a14a4ffb12fa91d9fb9ca323f
AnshulMindbody <anshul.jain@mindbodyonline.com>
2020-12-10 17:02:00 +0530

    addded changes

--- Motivator/Motivator/Dashboard/DashboardViewController.swift
+++ Motivator/Motivator/Dashboard/DashboardViewController.swift
@@ -6,18 +6,44 @@
 //
 
 import UIKit
+import Charts
 
 class DashboardViewController: UIViewController {
+    
+    @IBOutlet var pieChartView: PieChartView!
+    let players = ["Ozil", "Ramsey", "Laca"]
+    let goals = [6, 8, 26]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Dashboard"
-        
+        pieChartView.drawHoleEnabled = false
+        customizeChart(dataPoints: players, values: goals.map{ Double($0) })
 
         // Do any additional setup after loading the view.
     }
     
-
+    
+    func customizeChart(dataPoints: [String], values: [Double]) {
+      
+      // 1. Set ChartDataEntry
+      var dataEntries: [ChartDataEntry] = []
+      for i in 0..<dataPoints.count {
+        let dataEntry = PieChartDataEntry(value: values[i], label: dataPoints[i], data: dataPoints[i] as AnyObject)
+        dataEntries.append(dataEntry)
+      }
+      // 2. Set ChartDataSet
+        let pieChartDataSet = PieChartDataSet(entries: dataEntries, label: nil)
+      pieChartDataSet.colors = colorsOfCharts(numbersOfColor: dataPoints.count)
+      // 3. Set ChartData
+      let pieChartData = PieChartData(dataSet: pieChartDataSet)
+      let format = NumberFormatter()
+      format.numberStyle = .none
+      let formatter = DefaultValueFormatter(formatter: format)
+      pieChartData.setValueFormatter(formatter)
+      // 4. Assign it to the chart’s data
+      pieChartView.data = pieChartData
+    }
     /*
     // MARK: - Navigation
 
@@ -27,5 +53,17 @@ class DashboardViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+        
+        private func colorsOfCharts(numbersOfColor: Int) -> [UIColor] {
+          var colors: [UIColor] = []
+          for _ in 0..<numbersOfColor {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+          }
+          return colors
 
+        }
 }
